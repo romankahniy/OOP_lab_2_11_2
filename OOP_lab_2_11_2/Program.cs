b523a309a747c29f215b9cf3909a69245de0c2a5
Роман Данилевич <dwordchanel@gmail.com>
2020-05-03 15:08:21 +0300

    Add project files.

---
+++ OOP_lab_2_11_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP_lab_2_11_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double x = 0; x <= Math.PI; x += Math.PI / 20)
+            {
+                double y = Math.Sqrt(Math.Sqrt(Math.Tan(x) + 13));
+                Console.WriteLine("x = {0:f4}, y = {1:f4}", x, y);
+            }
+        }
+    }
+}
